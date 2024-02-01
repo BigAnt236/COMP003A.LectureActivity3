600093f6729be211fde22287949edbe283307a7d
anthonyalvarez <anthonyalvarez@LEM278-13>
2024-01-31 18:58:32 -0800

    Add project files.

---
+++ COMP003A.LectureActivity3/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Author: Anthony Alvarez
+ * Cours: COMP-003A
+ * Purpose: lecture Activity 3 Code
+ */
+
+namespace COMP003A.LectureActivity3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***********************");
+            Console.WriteLine("Conditional (Selection) Statements");
+        }
+    }
+}
