8c51e884061ed8a0aa269584a88e85ec9d2ccee8
Anthony Alvarez <anthonyalvarez@my.whccd.edu>
2024-01-31 19:03:40 -0800

    section 2lollloo

--- COMP003A.LectureActivity3/Program.cs
+++ COMP003A.LectureActivity3/Program.cs
@@ -10,8 +10,18 @@ namespace COMP003A.LectureActivity3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("***********************");
+            Console.WriteLine("***************************************");
             Console.WriteLine("Conditional (Selection) Statements");
+            Console.WriteLine("***************************************");
+
+            Console.WriteLine("1. single if statement");
+            Console.Write("\tEnter a whole positive number: ");
+            int inputNumber = Convert.ToInt32(Console.ReadLine()); // store user input as an integer
+
+            // single if statement
+            if(inputNumber % 2 == 0) {
+            
+            }
         }
     }
 }
