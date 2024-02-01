8b29cc8686b4af5e74bad934e4b898efa624b220
Anthony Alvarez <anthonyalvarez@my.whccd.edu>
2024-01-31 19:12:19 -0800

    Yo section 4

--- COMP003A.LectureActivity3/Program.cs
+++ COMP003A.LectureActivity3/Program.cs
@@ -19,8 +19,22 @@ namespace COMP003A.LectureActivity3
             int inputNumber = Convert.ToInt32(Console.ReadLine()); // store user input as an integer
 
             // single if statement
-            if(inputNumber % 2 == 0) {
-            
+            if (inputNumber % 2 == 0)
+            {
+                // this code block executes if the condition about is true
+                Console.WriteLine("\tinputNumber is even.");
+            }
+            Console.WriteLine("2. if-else staement");
+            // if-else statement
+            if (inputNumber % 2 == 0)
+            {
+                // this code block executes if the conditon about is true
+                Console.WriteLine("\tinputNumber is even.");
+            }
+            else
+            {
+                // this code block executes if the above condition is false
+                Console.WriteLine("\tinputNumber is odd.");
             }
         }
     }
